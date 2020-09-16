720afa491e0ca04a0f599590d430a775e231aa59
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2020-09-16 14:40:18 +0200

    this will create conflict

--- lib/pluggy/controllers/user_controller.ex
+++ lib/pluggy/controllers/user_controller.ex
@@ -1,39 +1,46 @@
 defmodule Pluggy.UserController do
   # import Pluggy.Template, only: [render: 2] #det här exemplet renderar inga templates
   import Plug.Conn, only: [send_resp: 3]
+  require IEx
 
-  def login(conn, params) do
-    username = params["username"]
-    password = params["pwd"]
+  alias Pluggy.Fruit
+  alias Pluggy.User
+  import Pluggy.Template, only: [render: 2, srender: 2]
+  import Plug.Conn, only: [send_resp: 3]
 
-     #Bör antagligen flytta SQL-anropet till user-model (t.ex User.find)
-    result =
-      Postgrex.query!(DB, "SELECT id, password_hash FROM users WHERE username = $1", [username],
-        pool: DBConnection.ConnectionPool
-      )
 
-    case result.num_rows do
-      # no user with that username
-      0 ->
-        redirect(conn, "/fruits")
-      # user with that username exists
-      _ ->
-        [[id, password_hash]] = result.rows
+  # def login(conn, params) do
+  #   username = params["username"]
+  #   password = params["pwd"]
 
-        # make sure password is correct
-        if Bcrypt.verify_pass(password, password_hash) do
-          Plug.Conn.put_session(conn, :user_id, id)
-          |> redirect("/fruits") #skicka vidare modifierad conn
-        else
-          redirect(conn, "/fruits")
-        end
-    end
-  end
+  #    #Bör antagligen flytta SQL-anropet till user-model (t.ex User.find)
+  #   result =
+  #     Postgrex.query!(DB, "SELECT id, password_hash FROM users WHERE username = $1", [username],
+  #       pool: DBConnection.ConnectionPool
+  #     )
 
-  def logout(conn) do
-    Plug.Conn.configure_session(conn, drop: true) #tömmer sessionen
-    |> redirect("/fruits")
-  end
+  #   case result.num_rows do
+  #     # no user with that username
+  #     0 ->
+  #       redirect(conn, "/fruits")
+  #     # user with that username exists
+  #     _ ->
+  #       [[id, password_hash]] = result.rows
+
+  #       # make sure password is correct
+  #       if Bcrypt.verify_pass(password, password_hash) do
+  #         Plug.Conn.put_session(conn, :user_id, id)
+  #         |> redirect("/fruits") #skicka vidare modifierad conn
+  #       else
+  #         redirect(conn, "/fruits")
+  #       end
+  #   end
+  # end
+
+  # def logout(conn) do
+  #   Plug.Conn.configure_session(conn, drop: true) #tömmer sessionen
+  #   |> redirect("/fruits")
+  # end
 
   # def create(conn, params) do
   # 	#pseudocode
@@ -43,6 +50,6 @@ defmodule Pluggy.UserController do
   #  	# redirect(conn, "/fruits")
   # end
 
-  defp redirect(conn, url),
-    do: Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
+  # defp redirect(conn, url),
+  #   do: Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
 end
