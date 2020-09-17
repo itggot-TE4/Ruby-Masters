855a578fc32714762cc56276d0ba198c5bf76201
Filip Liljenberg <filip.liljenberg1@Filips-MacBook-Pro.local>
2020-09-17 15:51:17 +0200

    new changes to school functionality

--- lib/pluggy/controllers/school_controller.ex
+++ lib/pluggy/controllers/school_controller.ex
@@ -1,8 +1,16 @@
 defmodule Pluggy.SchoolController do
+  require IEx
+
+  alias Pluggy.School
+  import Pluggy.Fruit
+  alias Pluggy.Fruit
+  import Plug.Conn, only: [send_resp: 3]
   def create(conn, params) do
-    name = params["name"]
+    School.create(conn, params)
+    redirect(conn, "/")
+  end
 
-
-    conn
+  def redirect(conn, url) do
+    Plug.Conn.put_resp_header(conn, "location", url) |> send_resp(303, "")
   end
 end
